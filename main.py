d95b7a8e0b1179aa07e5fdf89954d20abb345e76
Twan Roodenburg <twan.roodenburg@xs4all.nl>
2024-09-02 01:43:14 +0200

    wefqefwef

--- main.py
+++ main.py
@@ -215,10 +215,86 @@ def H4Opdr5():
 
 
 def PROG1_NS_Functies():
-    def standaardprijs(afstandKM):
-        if afstandKM < 0:
-            return 0
-        if afstandKM > 50:
-            return 15 + afstandKM * 0.60
-        return afstandKM * 0.80
-    ritprijs(leeftijd, weekendrit, afstandKM)
+
+    def standaardprijs(afstandKM):      # creert een functie met 1 parameter (een herhaalbaar stuk code met een variabele)
+        if afstandKM < 0:               # als de afstand kleiner is dan 0
+            return 0                    # betaal je niks
+        if afstandKM > 50:              # als de afstand groter is dan 50
+            return 15 + afstandKM * 0.60# betaal je 15 euro + 60 cent per kilometer
+        return afstandKM * 0.80         # anders betaal je 80 cent per kilometer
+    
+    def ritprijs(leeftijd, weekendrit, afstandKM):  # creert een functie met 3 parameters
+        prijs = standaardprijs(afstandKM)           # berekent de standaardprijs in de net beschreven functie
+        if leeftijd < 12 or leeftijd >= 65:         # als de leeftijd kleiner is dan 12 of groter of gelijk aan 65
+            if weekendrit:                          # als het een weekendrit is
+                return prijs * 0.65                 # betaal je 65% van de prijs
+            return prijs * 0.70                     # anders betaal je 70% van de prijs
+        if weekendrit:                              # als het een weekendrit is
+            return prijs * 0.60                     # betaal je 60% van de prijs
+        return prijs                                # anders betaal je de volledige prijs
+
+#nogmaals onderzoekend vermogen vind ik raar aangezien ik hier geen hulp of bron bij nodig had maar nogmaals hier is een python cursus https://www.youtube.com/watch?v=ix9cRaBkVe0
+
+
+def H5Opdr1():
+    def gemiddelde():
+        getallen = []
+        zin = input("Voer een willekeurige zin in: ")
+        for zin in zin.split(" "):
+            getallen.append(len(zin))
+        return sum(getallen) / len(getallen)
+
+
+def H5Opdr2():
+    def pretty_print(filepath):
+        prettyText = []
+        contents = open(filepath, "r")
+        for line in contents:
+            contents = line.split(",")
+            prettyText.append(f"{contents[1]} heeft kaartnummer: {contents[0]}")
+        contents.close()
+        return prettyText
+
+def H5Opdr3():
+    def getFileData(filepath):
+        contents = open(filepath, "r")
+        aantalLines = 0
+        nummers = []
+        for line in contents:
+            line = line.split(",")
+            aantalLines +=1
+            nummers.append(line[0])
+        contents.close()
+        return f"Deze file telt {aantalLines} regels\nHet grootste kaartnummer is: {max(nummers)} en dat staat op regel {nummers.index(max(nummers)) + 1}"
+
+def H5Opdr4():
+    import datetime 
+    vandaag = datetime.datetime.today() 
+    s = vandaag.strftime("%a %d %b %Y") 
+    t = vandaag.strftime("%H:%M:%S")
+
+    if os.file.exists("hardlopers.txt"):
+        file = open("hardlopers.txt", "a")
+    else:
+        file = open("hardlopers.txt", "w")
+
+    while True:
+        Input = "Voer naam in (stop met 'stop'): "
+        if Input == "stop":
+            break
+
+
+        file.write(f"{s}, {t}, {Input}\n")
+
+    file.close()
+
+def H5Opdr5():
+    def convert(celsius):
+        return celsius * 1.8 + 32
+
+    def table():
+        result = "  F       C\n"
+        for celsius in range(-30, 41, 10):
+            fahrenheit = convert(celsius)
+            result += f"{fahrenheit:6.1f}   {celsius:6.1f}\n"
+        return result
